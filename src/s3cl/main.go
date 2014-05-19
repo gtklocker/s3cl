3bb06e73701e62b6b02f980a40a63e5984ad7c19
jatsz <Jerry.Chou@Tobii.com>
2014-05-19 17:27:33 +0800

    workable version

--- src/s3cl/main.go
+++ src/s3cl/main.go
@@ -2,18 +2,208 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"os"
 	"io/ioutil"
+	"strings"
+	"github.com/goamz/goamz/aws"
+	"github.com/goamz/goamz/s3"
+	"github.com/jessevdk/go-flags"
 )
+
+type Options struct {
+    AccessKey string `short:"a" long:"access-key" description:"AccessKey"`
+    SecretKey string `short:"s" long:"secret-key" description:"SecretKey"`
+    Bucket    string `short:"b" long:"bucket" description:"s3 bucket"`
+    Prefix    string `long:"prefix" description:"prefix of s3 location"`
+    Output    string `short:"o" long:"output" description:"output file name"`
+}
+
+var opts = Options{}
 
 
 func main() {
-	fmt.Println("curl setup")
-	resp, err := http.Get("http://jatsz.org/")
+	args, err := flags.ParseArgs(&opts, os.Args)
 	if err != nil {
-		// handle error
+    	panic(err)
+    	os.Exit(1)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+
+	if len(args) < 2 {
+		help()	
+		return
+	}
+
+	if !setup_opts(&opts) {
+		return
+	}
+
+	subcmd := args[1]
+	if subcmd == "help" {
+		help()
+	} else if subcmd == "config" {
+		config()
+	} else if subcmd == "get" {
+		CmdGet(opts, args[2])
+	} else if subcmd == "put" {
+		CmdPut(opts, args[2], args[3])
+	} else if subcmd == "del" {
+		CmdDel(opts, args[2])
+	} else if subcmd == "mv" {
+		CmdMove(opts, args[2], args[3])
+	} else {  //default
+		help()
+	}
+
+	return
+}
+
+func setup_opts(opts *Options) bool {
+	if opts.AccessKey == "" {
+		opts.AccessKey = FileConfig("access_key")
+	}
+	if opts.AccessKey == "" {
+		opts.AccessKey = os.Getenv("S3CL_ACCESS_KEY")
+	}
+	if opts.AccessKey == "" {
+		fmt.Println("access key not found")
+		return false
+	}
+
+
+	if opts.SecretKey == "" {
+		opts.SecretKey = FileConfig("secret_key")
+	}
+	if opts.SecretKey == "" {
+		opts.SecretKey = os.Getenv("S3CL_SECRET_KEY")
+	}
+	if opts.SecretKey == "" {
+		fmt.Println("secret key not found")
+		return false
+	}
+
+	if opts.Bucket == "" {
+		opts.Bucket = FileConfig("bucket")
+	}
+	if opts.Bucket == "" {
+		opts.Bucket = os.Getenv("S3CL_BUCKET_DEFAULT")
+	}
+	if opts.Bucket == "" {
+		fmt.Println("bucket not found")
+		return false
+	}
+
+	return true
+}
+
+func FileConfig(key string)(val string) {
+	return ""
+}
+
+func print_usage() {
+	usage := `
+Usage:
+    s3cl get key
+    s3cl put key file_path
+    s3cl del key
+    s3cl mv  src_key dest_key
+    
+Options:
+    --access-key, -a    Access Key
+    --secret-key, -s    Secret Key
+    --bucket    , -b    Bucket Name
+    --prefix            Prefix of s3 location
+    --output    , -o    Output file name
+    `
+	fmt.Println(usage)
+}
+
+func help() {
+	print_usage()
+}
+
+func config() {
+	
+}
+
+func CmdGet(opts Options, key string) {
+	d, err := Get(opts, key)
+	if err != nil {
+		panic(err)
+	}
+	if opts.Output != "" {
+		err := ioutil.WriteFile(opts.Output, d, 0666)
+		if err != nil {
+	    	panic(err)
+		}
+	} else {
+		os.Stdout.Write(d)
+		os.Stdout.Sync()
+	}
+}
+
+func Get(opts Options, key string) (data []byte, err error){
+	auth := aws.Auth{AccessKey: opts.AccessKey, SecretKey: opts.SecretKey}
+	s3Instance := s3.New(auth, aws.Region{Name: "*", S3Endpoint: "http://s3.amazonaws.com"})
+	bkt := s3Instance.Bucket(opts.Bucket)
+
+	return bkt.Get(key)
+}
+
+
+func CmdPut(opts Options, key, file_path string) {
+	contType := "text/plain"
+	if strings.HasSuffix(file_path, ".xml") {
+		contType = "text/xml"
+	} else if strings.HasSuffix(file_path,".flv") {
+		contType = "video/x-flv"
+	}
+
+	d, err := ioutil.ReadFile(file_path)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	Put(opts, key, d, contType)
+	if err != nil {
+		panic(err)
+		return
+	}	
+}
+
+func Put(opts Options, key string, data []byte, contType string) error {
+	auth := aws.Auth{AccessKey: opts.AccessKey, SecretKey: opts.SecretKey}
+	s3Instance := s3.New(auth, aws.Region{Name: "*", S3Endpoint: "http://s3.amazonaws.com"})	
+	bkt := s3Instance.Bucket(opts.Bucket)
+
+	return bkt.Put(key, data, contType, s3.BucketOwnerFull, s3.Options{})
+}
+
+func CmdMove(opts Options, src_key, dest_key string) {
+	d, err := Get(opts, src_key)
+	if err != nil {
+		panic(err)
+	}
+
+	Put(opts, dest_key, d, "")
+	if err != nil {
+		panic(err)
+		return
+	}
+	Del(opts, src_key)
+}
+
+func Del(opts Options, key string) error{
+	auth := aws.Auth{AccessKey: opts.AccessKey, SecretKey: opts.SecretKey}
+	s3Instance := s3.New(auth, aws.Region{Name: "*", S3Endpoint: "http://s3.amazonaws.com"})
+	bkt := s3Instance.Bucket(opts.Bucket)
+
+	return bkt.Del(key)
+}
+
+func CmdDel(opts Options, key string) {
+	err := Del(opts, key)
+	if err != nil {
+		panic(err)
+	}
 }
